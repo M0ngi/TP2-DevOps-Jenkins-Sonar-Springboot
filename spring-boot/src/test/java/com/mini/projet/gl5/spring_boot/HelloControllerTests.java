c48bac5e63f1052f45912ed760d91e5f2d01b135
M0ngi <saidanemongi@gmail.com>
2024-11-06 21:50:52 +0100

    fix

--- spring-boot/src/test/java/com/mini/projet/gl5/spring_boot/HelloControllerTests.java
+++ spring-boot/src/test/java/com/mini/projet/gl5/spring_boot/HelloControllerTests.java
@@ -2,12 +2,14 @@ package com.mini.projet.gl5.spring_boot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import com.mini.projet.gl5.spring_boot.controllers.HelloController;
+
+@WebMvcTest(controllers = { HelloController.class })
 public class HelloControllerTests {
     @Autowired
     private MockMvc mockMvc;
